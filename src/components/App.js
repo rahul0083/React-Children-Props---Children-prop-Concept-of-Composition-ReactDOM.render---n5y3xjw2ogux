872e70ae8e7ea63d18abee2cb23dc1ecfc228bd1
Rahul Singh <rahulme0083@gmail.com>
2023-02-21 22:02:16 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,21 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
+ const [inpu1]=useState();
 
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
-    </div>
+    <ChildComponent input={
+     <input id='input' onChange={(e)=>{
+ let ans = e.target.value;
+ ans = Math.pow(ans, 2);
+ SetInp1(ans)
+ 
+}} />}
+/> 
+  
+    <p id='output'>{input1}</p>
+  </div>
   )
 }
 
